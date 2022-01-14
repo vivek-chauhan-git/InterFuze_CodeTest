6f527e7a4fc79773977479372fb42dac4dbf86ce
vivekchauhan <vivekchauhan@VIVEKCHAUHAN>
2022-01-14 18:27:50 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
